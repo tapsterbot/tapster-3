e6851f2a816abd1b5ea0c5211c4ea9207af4e672
mdrobot7 <mdrobot7@gmail.com>
2022-07-06 15:32:13 -0500

    calculator coordinates and code

--- demo/hello-t3/hello.py
+++ demo/hello-t3/hello.py
@@ -4,6 +4,22 @@ sys.path.append("..")
 import math
 import time
 import robot
+from calc import calculator, strToCalc
+from cv import checkAnswer
 
-def calculator(val): #val: a string of values to type on the calculator (i.e. "1+1=")
-    pass
+if len(sys.argv) > 1: #take in the serial port name from the args
+    PORT = sys.argv[1]
+else:
+    print("Please specify a port.")
+    raise SystemExit
+
+bot = robot.Robot(PORT, -15, -23.5, False, 0.09)
+
+#target: 0.7734
+calculator(bot, "c")
+calculator(bot, "1+1=")
+time.sleep(1)
+calculator(bot, "c")
+calculator(bot, "12.5-8.5*64+37+4869*1.5-9/10000=")
+
+#computer vision-y stuff here
