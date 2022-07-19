ed837fee5deddb8c73195c28f72a0aa885921766
mdrobot7 <mdrobot7@gmail.com>
2022-07-19 11:51:56 -0500

    just dance

--- demo/hello-t3/hello.py
+++ demo/hello-t3/hello.py
@@ -15,6 +15,7 @@ else:
     raise SystemExit
 
 bot = robot.Robot(PORT, -15, -23.5, False, 0.09)
+bot.go(0, 0, 0)
 
 #target: 0.1134
 calculator(bot, "cc")
@@ -22,9 +23,24 @@ calculator(bot, "1+1=")
 bot.go(0, 90, 53) #move the bot so the user can see
 time.sleep(2)
 
-calculator(bot, "c")
-calculator(bot, "27.0692+186.4*10/1.69420-126/10000=")
+while True:
+    calculator(bot, "cc")
+    calculator(bot, "27.0692+186.4*10/1.69420-126/10000=")
 
-bot.go(0, 90, 53) #move the delta linkages out of the way of the camera
+    bot.go(0, 90, 53) #move the delta linkages out of the way of the camera
 
-print(checkAnswer(0.1134))
+    if checkAnswer(0.1134):
+        bot.go(0, 0, 0)
+        time.sleep(0.1)
+        #Dance! (if it's correct)
+        for i in range(4):
+            for i in range(2):
+                bot.go(60, 0, 33)
+                bot.go(0, 0, -15)
+                bot.go(-60, 0, 33)
+                bot.go(0, 0, -15)
+            bot.go(0, 0, -15)
+            for i in range(8):
+                bot.go(0, 0, -15)
+                bot.go(0, 0, 10)
+        break
