aec5a1cbd6788818d160764a7886f1181ff5b2bb
mdrobot7 <mdrobot7@gmail.com>
2022-08-04 16:24:36 -0500

    commit just in case

--- demo/hello-no-coordinates-t3/pullCoordinates.py
+++ demo/hello-no-coordinates-t3/pullCoordinates.py
@@ -8,33 +8,74 @@ import pytesseract
 import imutils
 import numpy as np
 from cvUtils import cvUtils
+import json
 
 def pullCoordinates():
-    cvu = cvUtils()
-    cam = cv.VideoCapture(int(sys.argv[2]))
-    if not cam.isOpened():
-        print("Cannot open camera, make sure it is connected to the computer.")
-        exit()
+    pass
+
+def transformCoordinates(imgCoordinates): #transforms pixel coordinates in the video feed, returns robot coordinates
+    with open("coordinateData.json", "r") as file: coordinates = json.load(file)
+    print(coordinates)
     
+    for i in range(len(coordinates)): coordinates[i][1][1] = coordinates[i][1][1]*-1 #flip over x axis
+    print(coordinates)
+    translation = (coordinates[1][1][0] - coordinates[1][0][0], coordinates[1][1][1] - coordinates[1][0][1])
+    print(translation)
+    
+    for i in range(len(coordinates)): #translate the pixel coordinates so point [0] lines up on both coordinate systems
+        coordinates[i][1][0] -= translation[0]
+        coordinates[i][1][1] -= translation[1]
+
+    print(coordinates)
+
+    scaling = (stats.mean((coordinates[0][0][0], coordinates[2][0][0]))/stats.mean((coordinates[0][1][0], coordinates[2][1][0])), 
+               stats.mean((coordinates[0][0][1], coordinates[2][0][1]))/stats.mean((coordinates[0][1][1], coordinates[2][1][1])))
+    
+    coordinates[0][1][0] = coordinates[0][1][0]*scaling[0]
+    coordinates[0][1][1] = coordinates[0][1][1]*scaling[1]
+    coordinates[2][1][0] = coordinates[2][1][0]*scaling[0]
+    coordinates[2][1][1] = coordinates[2][1][1]*scaling[1]
+
+    print(scaling)
+    print(coordinates)
+
+    #imgCoordinates[1] = imgCoordinates[1]*-1
+
+
+def calibrateCamFeed(bot, cam):
+    cvu = cvUtils()
     cvu.clearImageBuffer(cam)
 
-    for i in range(100): #take in 100 frames
-        ret, frame = cam.read()
-        frame = cv.rotate(frame, cv.ROTATE_90_CLOCKWISE) #correct for camera rotation
+    ret, frame = cam.read()
 
-        #de-fish-eye here
-        frame = cvu.scaleDownFrame(frame, "calc-screen.jpg") #apply template matching and crop
-        frame = frame[20:frame.shape[0] - 25, 18:frame.shape[1] - 19] #remove edges (crop again)
-        
-        frame = cvu.preProcessFrame(frame, 45)
+    #if not os.path.exists("calibData.json"):
+    #    print("Calibration data does not exist. Running fisheye distortion calibrator...")
+    #    cvu.calibFishEyeRemover("./calib-images/*", "calibData.json", False)
+    #frame = cvu.removeFishEyeDistortion(frame, "calibData.json")
+    #cv.imshow("defisheye", frame)
 
-        cv.imshow("Frame", frame)
-        if cv.waitKey(1) == ord('q'): #REQUIRED LINE
-            break
-        cvu.processText(frame)
+    # the coordinates of the circle stickers in "robot space" is a known, guaranteed value.
+    # the values in the camera feed are unknown, and this list maps the two together. 
+    coordinates = [[[-2, 52], [0, 0]], [[-2, 1],[0, 0]], [[-2, -48], [0, 0]]]
+
+    frame = cv.cvtColor(frame, cv.COLOR_RGB2GRAY) #grayscale
+    frame = cv.rotate(frame, cv.ROTATE_90_CLOCKWISE) #fix rotation
+
+    frame = cvu.preProcessFrame(frame, [220, 255], False)
+
+    contours, hierarchy = cv.findContours(frame, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+    for i, c in enumerate(contours):
+        x, y, w, h = cv.boundingRect(c)
+        centerX = x + (w/2)
+        centerY = y + (h/2)
+        print(f"Contour {i}: ({centerX}, {centerY})")
+        coordinates[i][1] = [centerX, centerY]
+    
+    with open("coordinateData.json", "w") as file: json.dump(coordinates, file)
+
+    cam.release()
     cv.destroyAllWindows()
-    print("Best guess: " + cvu.evalText(100))
-    val = str(val)
-    val = val.replace(".", "") #remove periods and negative signs -- tesseract just isn't very good at picking these up
-    val = val.replace("-", "")
-    return (cvu.evalText(100) == val)
+
+
+
+transformCoordinates(None)
